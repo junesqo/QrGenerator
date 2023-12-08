b982a3837d815a97bface82a49a0447f3c9d4623
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2023-12-08 14:27:24 +0600

    scanner added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("com.github.androidmads:QRGenerator:1.0.1")
+    implementation("me.dm7.barcodescanner:zbar:1.8.4")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/kg/junesqo/qrgenerator/MainActivity.kt
+++ app/src/main/java/kg/junesqo/qrgenerator/MainActivity.kt
@@ -1,16 +1,22 @@
 package kg.junesqo.qrgenerator
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import androidmads.library.qrgenearator.QRGContents
 import androidmads.library.qrgenearator.QRGEncoder
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     var qrImage: ImageView? = null
     var generateButton: Button? = null
+    var scannerButton: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +24,11 @@ class MainActivity : AppCompatActivity() {
         generateButton = findViewById(R.id.generateButton)
         generateButton?.setOnClickListener {
             generatorQrCode("Image Processing")
+        }
+
+        scannerButton = findViewById(R.id.scannerButton)
+        scannerButton?.setOnClickListener {
+            checkCameraPermission()
         }
 
     }
@@ -29,6 +40,30 @@ class MainActivity : AppCompatActivity() {
             qrImage?.setImageBitmap(bMap)
         } catch (e: Exception){
 
+        }
+    }
+
+    private fun checkCameraPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 12)
+
+        } else {
+            startActivity(Intent(this, ScannerActivity::class.java))
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 12){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                startActivity(Intent(this,ScannerActivity::class.java))
+            }
         }
     }
 }
